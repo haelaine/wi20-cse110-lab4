c33a4aa7bc81eb3c6e621909676db795a7494ecb
haelaine <67618183+haelaine@users.noreply.github.com>
2021-01-29 23:24:12 -0800

    Update part1-question20.js

--- part1/part1-question20.js
+++ part1/part1-question20.js
@@ -1,3 +1,12 @@
+let statistics = {
+ redCars: 21, 
+ blueCars: 45,
+ greenCars: 12, 
+ raceCars: 5,
+ blackCars: 40, 
+ rareCars: 2
+};
+
 var intervalID = window.setInterval(func, 1000);
 function func()
 {
