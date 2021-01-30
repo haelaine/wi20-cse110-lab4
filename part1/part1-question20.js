f39d3a51228e964a9066c7f51942c8b6f471a618
haelaine <67618183+haelaine@users.noreply.github.com>
2021-01-29 23:25:15 -0800

    Update part1-question20.js

--- part1/part1-question20.js
+++ part1/part1-question20.js
@@ -1,13 +1,4 @@
-let statistics = {
- redCars: 21, 
- blueCars: 45,
- greenCars: 12, 
- raceCars: 5,
- blackCars: 40, 
- rareCars: 2
-};
-
-var intervalID = window.setInterval(func, 1000);
+var intervalID = scope.setInterval(func, 1000);
 function func()
 {
  	let d = new Date(); 
