2e58704ad06fa7ba7372ab2e63105cafec8b295b
haelaine <67618183+haelaine@users.noreply.github.com>
2021-01-29 23:18:57 -0800

    Update part1-question20.js

--- part1/part1-question20.js
+++ part1/part1-question20.js
@@ -1,8 +1,9 @@
 var intervalID = window.setInterval(func, 1000);
-function func(a, b)
+function func()
 {
  	let d = new Date(); 
    let time = d.toLocaleTimeString();
    console.log(time);
     
 }
+
